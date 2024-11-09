f31ced87a46c1b2f382d6a83b4437fed312c0613
Marcos <marcos.smz76@gmail.com>
2024-11-09 16:04:14 -0300

    auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,12 @@ export default async function HomePage() {
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map(
-          (image, index) => (
-            <div key={image.id + "-" + index} className="w-48 flex flex-col">
-              <img src={image.url} />
-              <div>{image.name}</div>
-            </div>
-          ),
-        )}
+        {[...images, ...images, ...images].map((image, index) => (
+          <div key={image.id + "-" + index} className="flex w-48 flex-col">
+            <img src={image.url} />
+            <div>{image.name}</div>
+          </div>
+        ))}
       </div>
     </main>
   );
