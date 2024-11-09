770115b2f170797dd562997e1cf476360275ce3f
Marcos <marcos.smz76@gmail.com>
2024-11-09 16:26:46 -0300

    auth to landpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
-
   return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap gap-4">
         {[...images, ...images, ...images].map((image, index) => (
           <div key={image.id + "-" + index} className="flex w-48 flex-col">
             <img src={image.url} />
@@ -17,6 +16,18 @@ export default async function HomePage() {
           </div>
         ))}
       </div>
+  )
+}
+
+export default async function HomePage() {
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">Please Sign In above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images></Images>
+      </SignedIn>
     </main>
   );
 }
